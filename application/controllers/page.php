2de53705f697f079fa4d742e184b6d7e50e9eb47
airyland <i@mao.li>
2012-12-28 13:59:07 +0800

    page added enabled

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -7,21 +7,16 @@ class Page extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('pages');
+        $this->load->library('s');
     }
 
     function index($slug = '') {
-      if($slug){
-          if($this->pages->get_page_info($slug)){
-              $this->load->library('s');
-              $this->s->display('page.html');
-          }else{
-              echo '页面不存在哦';
-          }
-          
-      }else{
-          echo '页面还不存在哦';
-      }
+        $path=APPPATH.'templates/page/'.$slug.'.html';
+     if(file_exists($path)){
+         $this->s->display("page/{$slug}.html");
+     }else{
+         show_404();
+     }
     }    
 }
 
