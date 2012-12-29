b07d267d031864d2dfa2f5ab667730b201b43498
airyland <i@mao.li>
2012-12-29 16:10:02 +0800

    refactor avatar controller

---
+++ application/controllers/avatar.php
@@ -0,0 +1,78 @@
+<?php
+!defined('BASEPATH') && exit('No direct script access allowed');
+/**
+ * NodePrint
+ *
+ * 基于HTML5及CSS3的轻论坛程序
+ *
+ * NodePrint is an open source BBS System built on PHP and MySQL.
+ *
+ * @package	NodePrint
+ * @author		airyland <i@mao.li>
+ * @copyright	Copyright (c) 2012, mao.li.
+ * @license	MIT
+ * @link		https://github.com/airyland/nodeprint
+ * @version	0.0.5
+ */
+
+/**
+ * Avatar handler
+ *
+ * @subpackage Controller
+ * for easier user of avatar, we do not directly output the image, instead we can just user '/avatar/$Id/$size'as image's src, if user does not upload his own avatar, then output the default image '0.png';
+ * the support sizes are 20, 48, 73.
+ */
+class Avatar extends CI_Controller
+{
+
+    public $imgdata;
+    public $imgsrc;
+    public $default='0.png';
+
+    function __construct()
+    {
+        parent::__construct();
+        header('Pragma: public');
+        header('Cache-Control: max-age='.(86400*30));
+        header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 86400*30));
+        header('Content-Type: image/png');
+    }
+
+    function index($id, $size)
+    {
+        $size_map=array(
+            '20'=>'s',
+            '48'=>'m',
+            '73'=>'l'
+        );
+        $this->imgsrc='img/avatar/'.$size_map[$size].'/'.$id.'.png';
+        if(!$this->img_exists()){
+            $this->imgsrc='img/avatar/'.$size_map[$size].'/'.$this->default;
+        }
+
+        $headers = apache_request_headers();
+        $mtime=filemtime($this->imgsrc);
+        if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) ==$mtime )) {
+            // Client's cache IS current, so we just respond '304 Not Modified'.
+            header('Last-Modified: '.gmdate('D, d M Y H:i:s', $mtime).' GMT', true, 304);
+        } else {
+            // Image not cached or cache outdated, we respond '200 OK' and output the image.
+            header('Last-Modified: '.gmdate('D, d M Y H:i:s', $mtime).' GMT', true, 200);
+            header('Content-Length: '.filesize($this->imgsrc));
+            header('Content-Type: image/png');
+           echo $this->img2data();
+        }
+    }
+
+    public function img2data()
+    {
+        return $this->imgdata = fread(fopen($this->imgsrc, 'rb'), filesize($this->imgsrc));
+    }
+
+    public function img_exists(){
+        return file_exists($this->imgsrc);
+    }
+}
+
+/* End of file avatar.php */
+/* Location: ./application/controllers/avatar.php */
