8c9b44be312d772362038e31fc8d805896a1b9f4
airyland <i@mao.li>
2012-12-29 16:10:14 +0800

    add site validation config file

---
+++ application/config/validation.php
@@ -0,0 +1,23 @@
+<?php
+//topic
+$config['topic_title_min_length'] = 2;
+//user name
+$config['user_name_min_length'] = 3;
+$config['user_name_max_length'] = 6;
+//user password
+$config['user_pwd_min_length'] = 6;
+$config['user_pwd_max_length'] = 16;
+//image size
+$config['img_max_size'] = 2 * 1024;
+$config['img_max_width'] = 1024;
+$config['img_max_height'] = 1024;
+$config['img_upload_path'] = APPPATH . '../img/avatars/';
+
+
+
+
+
+
+
+
+
