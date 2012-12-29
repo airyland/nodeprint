b0640710c3c9bcdc5e7b985519721e1462839ead
airyland <i@mao.li>
2012-12-29 16:10:28 +0800

    add global site config file

---
+++ application/config/site.php
@@ -0,0 +1,10 @@
+<?php
+//feed post no
+$config['feed_post_no']=15;
+//feed cache expire time
+$config['feed_expire_time']=0;
+//site email, show in the rss
+$config['site_email']='i@mao.li';
+//page directory
+$config['page_directory']='./application/templates/page/';
+//
