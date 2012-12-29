e5e45a73d26946be8031bfb383e4dad5d21eac61
airyland <i@mao.li>
2012-12-29 15:52:29 +0800

    add mb_strlen fallback

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -15,23 +15,18 @@ function current_time() {
 function get_random_strings($length, $valid_chars = 'abcdefghjkmnpqrsuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789') {
     // start with an empty random string
     $random_string = "";
-
     // count the number of chars in the valid chars string so we know how many choices we have
     $num_valid_chars = strlen($valid_chars);
-
     // repeat the steps until we've created a string of the right length
     for ($i = 0; $i < $length; $i++) {
         // pick a random number from 1 up to the number of valid chars
         $random_pick = mt_rand(1, $num_valid_chars);
-
         // take the random character out of the string of valid chars
         // subtract 1 from $random_pick because strings are indexed starting at 0, and we started picking at 1
         $random_char = $valid_chars[$random_pick - 1];
-
         // add the randomly-chosen char onto the end of our string so far
         $random_string .= $random_char;
     }
-
     // return our finished random string
     return $random_string;
 }
@@ -260,3 +255,22 @@ function load_lang() {
     include(APPPATH . 'helpers/lang_helper.php');
     return lang($config['lang']);
 }
+
+if(!function_exists('mb_strlen')){
+    function mb_strlen( $str, $enc = '' ) {
+        $counts = count_chars( $str );
+        $total = 0;
+        // Count ASCII bytes
+        for( $i = 0; $i < 0x80; $i++ ) {
+            $total += $counts[$i];
+        }
+        // Count multibyte sequence heads
+        for( $i = 0xc0; $i < 0xff; $i++ ) {
+            $total += $counts[$i];
+        }
+        return $total;
+    }
+
+}
+
+
