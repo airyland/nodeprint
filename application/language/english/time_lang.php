40518991ce20b8a70556ba62275c42e7e0ffba24
李政 <airyland@qq.com>
2013-01-02 22:33:07 +0800

    add relativeTime language files

---
+++ application/language/english/time_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang["%d day ago"] = "yesterday";
+$lang["%d days ago"] = "%d days ago";
+$lang["%d hour ago"] = "1 hour ago";
+$lang["%d hours ago"] = "%d hours ago";
+$lang["%d minute ago"] = "1 minute ago";
+$lang["%d minutes ago"] = "%d minutes ago";
+$lang["%d month ago"] = "1 month ago";
+$lang["%d months ago"] = "%d months ago";
+$lang["%d second ago"] = "1 second ago";
+$lang["%d seconds ago"] = "%d seconds ago";
+$lang["%d week ago"] = "last week";
+$lang["%d weeks ago"] = "%d weeks ago";
+$lang["%d year ago"] = "last year";
+$lang["%d years ago"] = "%d years ago";
+
+/* End of file time_lang.php */
+/* Location: ./system/language/english/time_lang.php */
---
+++ application/language/zh/time_lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang["%d day ago"] = "昨天";
+$lang["%d days ago"] = "%d 天前";
+$lang["%d hour ago"] = "1 小时前";
+$lang["%d hours ago"] = "%d 小时前";
+$lang["%d minute ago"] = "1 分钟前";
+$lang["%d minutes ago"] = "%d 分钟前";
+$lang["%d month ago"] = "1 个月前";
+$lang["%d months ago"] = "%d 个月前";
+$lang["%d second ago"] = "1 秒前";
+$lang["%d seconds ago"] = "%d 秒前";
+$lang["%d week ago"] = "上周";
+$lang["%d weeks ago"] = "%d 周前";
+$lang["%d year ago"] = "去年";
+$lang["%d years ago"] = "%d 年前";
+
+/* End of file time_lang.php */
+/* Location: ./system/language/zh/time_lang.php */
