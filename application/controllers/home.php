3dc02285584059cd664856111d177d2418631679
李政 <airyland@qq.com>
2013-01-03 01:51:54 +0800

    home page site tabs now works

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -24,6 +24,11 @@
 class Home extends CI_Controller {
 
     public $tab;
+    private $available_tab=array('following-node','following-member');
+    public $topics;
+    private $curr_user;
+    public $limit;
+    public $count;
 
     /**
      * 构造器
@@ -37,19 +42,42 @@ class Home extends CI_Controller {
         $this->load->model('nodes');
         $this->load->model('site');
         $this->load->model('user');
+        $this->limit = $this->configs->get_config_item('topic_no');
+        $this->curr_user = $this->auth->get_user();
         $this->tab = $this->input->get('tab');
-        function time_ago($paras) {
-            return relative_time(strtotime($paras['time']));
+    }
+
+    /**
+    * query topics
+    *
+    */
+    public function get_topic(){
+        if($this->tab&&in_array($this->tab,$this->available_tab)&&$this->tab!=='all'){
+            switch($this->tab){
+                case 'following-member':
+                    $this->load->model('follow');
+                    $this->topics = $this->follow->get_following_user_stream($this->curr_user['user_id'], false, 1, $this->limit);
+                break;
+
+                case 'following-node':
+                $this->load->model('nodes');
+               // $node = $this->user->get_user_fav_node($this->curr_user['user_id']);
+                $this->topics = $this->nodes->get_user_fav_node_post($this->curr_user['user_id'], 1, $this->limit, false, 'post_last_comment');
+                //$count_post = $this->nodes->get_user_fav_node_post($this->curr_user['user_id'], 0, 0, true);
+                break;
+            }
+        }else{
+            $query_string="order_by=post_last_comment&no={$this->limit}&all=0";
+            $this->topics=$this->post->query_post($query_string);
         }
-        $this->s->registerPlugin('function', 'time_ago', 'time_ago');
+        return $this->topics;
     }
 
     /**
      * 首页默认页面 Controller
      */
-    function index() {
+    public function index() {
         $lang = load_lang();
-        $limit = $this->configs->get_config_item('topic_no');
         $show_status=$this->configs->get_config_item('show_status');
         if (!$this->s->isCached("index.html")) {
             $nodes = $this->nodes->list_node(1, 0, 'node_id', 'DESC', 1, 15);
@@ -60,7 +88,7 @@ class Home extends CI_Controller {
             }
             $this->s->assign(array(
                 'title' => $lang['index'],          
-                'post' => $this->post->query_post("order_by=post_last_comment&no={$limit}&all=0"),
+                'post' => $this->get_topic(),
                 'nodes' => $nodes,
                 'show_status'=>$show_status,
                 'status' => $this->site->get_site_status(),
