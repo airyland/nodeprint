87f0cb3cfd44ea969e782b45a3c4b882baccb507
airyland <airyland@qq.com>
2013-01-06 16:56:56 +0800

    js update:sync node info caches every day

--- js/cron.js
+++ js/cron.js
@@ -1,1 +1,37 @@
+/**
+ * tool for cron job
+ * @author airyland <i@mao.li>
+ */
 
+// update node info cache
+$(function () {
+    var lastNodeCacheSync = store.get('lastNodeCacheSync'),
+        formatDate = function (date) {
+            var year = date.getFullYear(),
+                month = date.getMonth() + 1,
+                day = date.getDate();
+            return year + '-' + month + '-' + day;
+        },
+        today = formatDate(new Date),
+        log = true;
+    $.log = function ($string) {
+        log && window.console && console.log($string);
+    }
+    if (today !== lastNodeCacheSync) {
+        $.log('sync begin');
+        $.get('/api/nodes/list', function (data) {
+            for (var i = 0, len = data['nodes'].length; i < len; i++) {
+                for (var j = 0, childLen = data['nodes'][i]['child_node'].length; j < childLen; j++) {
+                    var node = data['nodes'][i]['child_node'][j];
+                    store.set('/node/' + node['node_slug'], node);
+                }
+            }
+            store.set('lastNodeCacheSync', today);
+            $.log('sync end');
+        }, 'json');
+    } else {
+        $.log('synced today');
+    }
+
+
+});
