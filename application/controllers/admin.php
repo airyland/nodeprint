a520c22b6ca69553a85070e83ae678aff40af9d6
李政 <i@mao.li>
2013-01-31 16:20:16 +0800

    templates update

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,5 +1,6 @@
-<?php  !defined('BASEPATH') && exit('No direct script access allowed');
-header("Content-type:text/html;charset='utf-8'");
+<?php
+
+!defined('BASEPATH') && exit('No direct script access allowed');
 
 /**
  * Admin Controller
@@ -22,14 +23,14 @@ class Admin extends CI_Controller {
         $config = $this->configs->get_config();
         $this->load->model('site');
         $status = $this->site->get_site_status();
-        $admins=$this->admins->get_admin();
+        $admins = $this->admins->get_admin();
         $this->s->assign(array(
             's' => $status,
-            'admins'=>$admins,
+            'admins' => $admins,
             'lang' => $lang,
             'title' => 'Dashboard',
             'config' => $config,
-            'last_backup_file'=>$this->admins->get_last_backup_file()
+            'last_backup_file' => $this->admins->get_last_backup_file()
         ));
         $this->s->display('admin/admin_index.html');
     }
@@ -52,7 +53,7 @@ class Admin extends CI_Controller {
                 //$this->load->model('metas_mdl');
                 $this->load->library('plugin');
                 echo $this->plugin->trigger('Widget::Categories', '<li><a href="{permalink}" title="{description}">{title} [{count}]</a></li>');
-				echo 'fuck';
+                echo 'fuck';
                 foreach ($plugins as $plugin) {
                     if (!in_array($plugin, $activated_plugins)) {
                         $deactivated_plugins[] = $plugin;
@@ -87,14 +88,14 @@ class Admin extends CI_Controller {
                 if ($id == '') {
                     $node = $this->nodes->list_node(0, 0, 'node_id', 'DESC', 1, 0);
                     $parent_node = $this->nodes->list_node(1, 0, 'node_id', 'DESC', 1, 0);
-                    $nodes=$this->nodes->get_all_nodes();
+                    $nodes = $this->nodes->get_all_nodes();
                     $this->s->assign(array(
                         'title' => '节点管理-网站设置',
                         'p_node' => $parent_node,
                         'lang' => $lang,
                         'user' => $user,
                         'node' => $node,
-                        'nodes'=>$nodes
+                        'nodes' => $nodes
                     ));
                     $this->s->display('admin/admin_nodes.html');
                 } else {
@@ -124,9 +125,9 @@ class Admin extends CI_Controller {
                 break;
 
             case 'topics':
-                $page=$this->input->get('page');
-                if(!$page){
-                    $page=1;
+                $page = $this->input->get('page');
+                if (!$page) {
+                    $page = 1;
                 }
                 $this->load->library('dpagination');
                 $this->load->model('nodes');
@@ -153,7 +154,7 @@ class Admin extends CI_Controller {
                         'user' => $user,
                         'node' => $node,
                         'post' => $post,
-                        'pagebar'=>$this->dpagination->getOutput()
+                        'pagebar' => $this->dpagination->getOutput()
                     ));
                     $this->s->display('admin/topics.html');
                 } else {
@@ -165,69 +166,70 @@ class Admin extends CI_Controller {
              * Page Management
              */
             case 'pages':
-                      $this->load->helper('directory');
-                      $this->config->load('site');
-                      $directory=$this->config->item('page_directory');
-                      $all_files=array_filter(directory_map($directory,1),function($item){return strpos($item,'html');});
-                      $files=array();
-                      foreach($all_files as $key => $val){
-                          $files[$key]['name']=$val;
-                          $files[$key]['time'] = filemtime($directory.$val);    
-                      }
-                      unset($all_files);
-                if ($id == '') {                 
+                $this->load->helper('directory');
+                $this->config->load('site');
+                $directory = $this->config->item('page_directory');
+                $all_files = array_filter(directory_map($directory, 1), function($item) {
+                            return strpos($item, 'html');
+                        });
+                $files = array();
+                foreach ($all_files as $key => $val) {
+                    $files[$key]['name'] = $val;
+                    $files[$key]['time'] = filemtime($directory . $val);
+                }
+                unset($all_files);
+                if ($id == '') {
                     $this->s->assign(array(
                         'title' => '页面管理',
                         'user' => get_user(),
-                        'files'=>$files,
-                        'count'=>count($files)
+                        'files' => $files,
+                        'count' => count($files)
                     ));
                     $this->s->display('admin/admin_pages.html');
                 } else {
-                    $page=$this->input->get('p');
-                    if(file_exists($directory.$page)){
-                         $this->s->assign(array(
-                        'title' => '页面管理',
-                        'user' => get_user(),
-                        'files'=>$files,
-                        'count'=>count($files),
-                        'file_content'=>  file_get_contents($directory.$page)
-                    ));
+                    $page = $this->input->get('p');
+                    if (file_exists($directory . $page)) {
+                        $this->s->assign(array(
+                            'title' => '页面管理',
+                            'user' => get_user(),
+                            'files' => $files,
+                            'count' => count($files),
+                            'file_content' => file_get_contents($directory . $page)
+                        ));
 
-                    $this->s->display('admin/admin_pages_edit.html');
-                    }else{
-                        show_error('文件不存在哦',404);
+                        $this->s->display('admin/admin_pages_edit.html');
+                    } else {
+                        show_error('文件不存在哦', 404);
                     }
-                   
                 }
                 break;
 
-                /**
-                * 清除Smarty缓存
-                */
-                case 'clearCache':
+            /**
+             * 清除Smarty缓存
+             */
+            case 'clearCache':
                 $this->load->library('s');
                 $this->s->clearAllCache();
                 break;
-            
+
             case 'backup':
                 $this->admins->backup();
                 break;
             case 'tools':
                 $this->s->assign(array(
-                    'title'=>'Admin tools'
+                    'title' => 'Admin tools'
                 ));
                 $this->s->display('admin/admin_tools.html');
                 break;
             case 'tool':
-                switch($id){
+                switch ($id) {
                     //clear smarty compiled templates
                     case 'clearCompiledTemplate':
                         $this->load->library('s');
                         $this->s->clearCompiledTemplate();
                         redirect('/admin/tools#clearCompiledTemplate_success');
                         break;
-                    
+
                     case 'clearAllCache':
                         $this->load->library('s');
                         $this->s->clearAllCache();
@@ -235,7 +237,7 @@ class Admin extends CI_Controller {
                         break;
                 }
                 break;
-
         }
     }
+
 }
