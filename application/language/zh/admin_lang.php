ffcad77102a977d31b959eafb950b36b8ebaf582
airyland <airyland@qq.com>
2013-02-26 09:45:46 +0800

    add admin language file

---
+++ application/language/zh/admin_lang.php
@@ -0,0 +1,13 @@
+<?php
+$lang["add_node"] = "添加节点";
+$lang["plugin management"] = "插件管理";
+$lang["node management"] = "节点管理";
+$lang["user management"] = "用户管理";
+$lang["topic management"] = "帖子管理";
+$lang["page management"] = "页面管理";
+$lang["site settings"] = '网站设置';
+$lang['file does not exist'] = '文件不存在哦';
+
+
+/* End of file admin_lang.php */
+/* Location: ./system/language/zh/admin_lang.php */
