22a748845305bb412f4026fc57bb158d4dd97b9f
airyland <airyland@qq.com>
2013-03-08 14:44:36 +0800

    custom 403 error page

---
+++ application/errors/error_403.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>米有权限查看</title>
+<style>
+*{margin:0;padding:0}
+body{font-family:"微软雅黑";}
+img{border:none}
+a *{cursor:pointer}
+ul,li{list-style:none}
+table{table-layout:fixed;}
+table tr td{word-break:break-all; word-wrap:break-word;}
+.cf:after{content: ".";display: block;height: 0;font-size: 0;clear:both;visibility: hidden;}
+.cf{zoom: 1;clear:both}
+.bg{width:100%;position:absolute;top:0;left:0;height:600px;overflow:hidden}
+.cont{margin:0 auto;width:500px;line-height:20px;}
+.c1{height:360px;text-align:center}
+.c1 .img1{margin-top:180px}
+.c1 .img2{margin-top:165px}
+.cont h2{text-align:center;color:#555;font-size:18px;font-weight:normal;height:35px}
+.c2{height:35px;text-align:center}
+.c2 span{display:inline-block;margin:0 4px;font-size:14px;height:23px;color:#626262;padding-top:1px;text-decoration:none;text-align:left}
+.c2 span:hover{color:#626262;text-decoration:none;}
+.c3{height:180px;text-align:center;color:#999;font-size:12px}
+#bf{position:absolute;top:269px;left:0;width:100%}
+.bf1{margin:0 auto;width:99px;padding-left:32px}
+.bd{height:600px;overflow:hidden}
+#box{position:absolute;top:165px;left:0;width:100%;text-align:center}
+.bf1{margin:0 auto;width:99px;padding-left:32px}
+</style>
+</head>
+<body>
+<div class="bg">
+	<div class="cont">
+		<div class="c1"><img src="/img/404.png" class="img1" /></div>
+		<h2>亲，你没有权限查看哦</h2>
+		<div class="c2">
+			<a href="#"><span class="re" >我不信，再试一次</span></a>
+			<a href="/"><span class="home">去网站首页</span></a>
+		</div>
+		<div class="c3">HTTP Error 403 
+		<br>
+		</div>
+	</div>
+</div>
+</body>
+</html>
