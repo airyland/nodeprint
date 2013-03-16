fbc0caa4b666b20de46b344d27a542bab8ed3c2d
李政 <airyland@qq.com>
2013-03-16 23:51:54 +0800

    list user's oAuth binding list

--- application/controllers/settings.php
+++ application/controllers/settings.php
@@ -26,22 +26,30 @@ class Settings extends CI_Controller {
         'google' => 'Google'
         );
 
-    function index() {
+    protected $user_info;
+
+    public function index() {
         global $lang;
         $this->auth->check_login();
         $this->load->model('user');
         $user = $this->auth->get_user();
-        $user_info = $this->user->get_user_profile($user['user_id'],'user_id');
+        $this->user_info = $this->user->get_user_profile($user['user_id'],'user_id');
         $this->load->library('s');
         $this->s->assign(array(
             'title' => $lang['settings'],
             'lang' => $lang,
             'avatar' => get_avatar($user['user_id']),
-            'gravatar' => 'http://www.gravatar.com/avatar/'.md5(strtolower(trim($user_info['user_email']))),
+            'gravatar' => 'http://www.gravatar.com/avatar/'.md5(strtolower(trim($this->user_info['user_email']))),
             'timestamp' => time(),
-            'user_from' => $this->service_map[strtolower($user_info['user_from'])]
+            'user_from' => $this->service_map[strtolower($this->user_info['user_from'])],
+            'user_oauth' => $this->get_oauth_binding_list()
         ));
         $this->s->display('user/user_settings.html');
+    }
+
+    private function get_oauth_binding_list(){
+        $rs = $this->db->get_where('oauth',array('user_id'=>$this->user_info['user_id']));
+        return $rs->result_array();
     }
 
 }
