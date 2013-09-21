a27e5204dc366511cb03e91662ed82f47e19b8b3
airyland <airyland@qq.com>
2013-09-21 14:24:53 +0800

    add grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,29 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {},
-        cssmin: {}
+        cssmin: {
+            dist: {
+                files: {
+                    'css/style.min.css': ['css/style.css']
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'css/source/style.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/source/*.scss',
+            tasks: ['default']
+        },
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // 执行默认任务
-    grunt.registerTask('default', ['uglify', 'cssmin']);
+    grunt.registerTask('default', ['sass:dist', 'cssmin:dist']);
 }
