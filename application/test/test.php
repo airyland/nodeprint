92e962525b90b4b5d638a77f760ede11549cd39e
李政 <airyland@qq.com>
2013-09-22 10:34:18 +0800

    create context

--- application/test/test.php
+++ application/test/test.php
@@ -3,6 +3,8 @@
  * just a simple test
  *
  */
-echo file_get_contents('http://nodeprint.com');
-echo file_get_contents('http://127.0.0.1');
+ 
+$context = stream_context_create(array('http'=>array('ignore_errors'=>true)));
+echo file_get_contents('http://nodeprint.com', false, $context);
+echo file_get_contents('http://127.0.0.1', false, $context);
 exit(0);
