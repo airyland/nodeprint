b79f54d3fe825ca724db0c9da0c8e18d9cbd478a
airyland <airyland@qq.com>
2013-09-22 23:15:53 +0800

    add email default setting file

---
+++ application/config/email_default.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+            'smtp_host' => 'ssl://smtp.qq.com',
+            'smtp_user' => 'test@nodeprint.com',
+            'smtp_pass' => 'test',
+            'smtp_port' => 465,
+            'protocol' => 'smtp',
+            'newline' => "\r\n",
+            'mailtype' => 'html'
+        );
